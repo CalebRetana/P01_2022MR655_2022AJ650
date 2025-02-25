c7e5bf2a1dc2d207ecbdda794eb70af4a559e71f
asensio01 <guillermo.asensio@catolica.edu.sv>
2025-02-24 18:19:41 -0600

    Context

---
+++ P01_2022MR655_2022AJ650/Models/ParqueoContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P01_2022MR655_2022AJ650.Models
+{
+    public class ParqueoContext : DbContex
+    {
+        public ParqueoContext() { }
+    }
+}
